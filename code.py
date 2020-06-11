74bc5f6ee25d982fabdf9c30b2f803d565de5642
Manan Nahata <killgear0@gmail.com>
2020-06-11 22:21:56 +0530

    Stream RGB data for pixels from host
    Co-authored-by: Saurabh <saurabh.chaturvedi63@gmail.com>

--- code.py
+++ code.py
@@ -6,14 +6,16 @@ import sys
 import random
 
 def non_blocking_read():
+    rgb_s = []
     s = ""
-    while supervisor.runtime.serial_bytes_available:
-        s += sys.stdin.read(1)
-    return s.strip()
+    while supervisor.runtime.serial_bytes_available and s != '\n':
+        s = sys.stdin.read(1)
+        rgb_s.append(s)
+    return ''.join(rgb_s).strip()
 
 def parse_inp(s):
     return [tuple(int(p) for p in t.split(' ')) for t in s.split(',')]
-[]
+
 def light_up(rgb_out):
     # TODO: Varying brightness
     cp.pixels.brightness = 0.01
@@ -27,5 +29,10 @@ def random_rgb():
 
 while True:
     # print('circuit-cinema')
-    light_up(parse_inp((random_rgb())))
-    time.sleep(0.5)
+    #light_up(parse_inp((random_rgb())))
+    # time.sleep(0.5)
+    output = non_blocking_read()
+    if output:
+        print(repr(output), end='-------')
+        light_up(parse_inp((output)))
+    time.sleep(0.2)
